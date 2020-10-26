df68c143d7fc0b3dbbb9c8bec88299a479e49489
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2020-10-26 15:51:48 +0100

    add check on tag presence

--- bin/build-push.sh
+++ bin/build-push.sh
@@ -1,6 +1,11 @@
 #/bin/bash -e
 
 DOCKERVERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+if [ -z "$DOCKERVERSION" ]
+then
+    echo -e "\n\e[1m\e[31mERROR! Please tag this repository, like: git tag 0.0.1\e[0m\e[0m"
+    exit 1
+fi
 DOCKERUSER=gabrieletassoni
 DOCKERTAG=$DOCKERUSER/vscode-devcontainers-thecore:$DOCKERVERSION
 
