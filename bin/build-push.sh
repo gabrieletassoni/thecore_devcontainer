76fcfdb5b947ae3bdf600f6a4b5b2ca04c69cb00
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2020-11-03 14:37:07 +0000

    New Version 2.2.0

--- bin/build-push.sh
+++ bin/build-push.sh
@@ -1,11 +1,42 @@
 #/bin/bash -e
 
-DOCKERVERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
-if [ -z "$DOCKERVERSION" ]
+CURRENTVERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+if [ -z "$CURRENTVERSION" ]
 then
-    echo -e "\n\e[1m\e[31mERROR! Please tag this repository, like: git tag 0.0.1\e[0m\e[0m"
-    exit 1
+    git tag 0.0.1
+    CURRENTVERSION=0.0.1
 fi
+echo "Currently this repository is at version $CURRENTVERSION"
+
+select T in "Major" "Minor" "Patch"
+do
+  echo "Selected ${T}"
+  if ! [ -z "${T}" ]
+  then
+    TYPE="$T"
+    break
+  fi
+done
+
+case $T in
+    Major ) 
+        SWITCH="-M"
+        ;;
+    Minor ) 
+        SWITCH="-m"
+        ;;
+    Patch ) 
+        SWITCH="-p"
+        ;;
+esac
+
+echo "SWTICH: $SWITCH"
+
+DOCKERVERSION=$(bin/increment_version.sh $SWITCH $CURRENTVERSION)
+echo "New Version: $DOCKERVERSION"
+git add . -A
+git commit -a -m "New Version $DOCKERVERSION"
+git tag $DOCKERVERSION
 DOCKERUSER=gabrieletassoni
 DOCKERTAG=$DOCKERUSER/vscode-devcontainers-thecore:$DOCKERVERSION
 
