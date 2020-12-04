ca67b96d2d6c0ff87014e9e24b887b40503d4d8f
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2020-12-04 08:27:52 +0000

    Cleanup (but it's not working still

--- bin/build-push.sh
+++ bin/build-push.sh
@@ -1,8 +1,8 @@
-#/bin/bash -e
+#!/bin/bash -e
 
 source bin/build.sh
 echo "Login to docker hub"
 docker login
-[[ -n "$DOCKERTAG" ]] && docker push $DOCKERTAG
+[[ -n "$DOCKERTAG" ]] && docker push "$DOCKERTAG"
 
 exit 0
