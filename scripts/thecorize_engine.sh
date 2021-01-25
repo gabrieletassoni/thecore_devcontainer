1e9624a149e2f9ebfe4294df113cc3db64075064
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-01-25 15:51:07 +0000

    New Version 2.8.5

--- scripts/thecorize_engine.sh
+++ scripts/thecorize_engine.sh
@@ -2,9 +2,9 @@
 
 if [[ $1 == "--help" ]]
 then
-echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (API only):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 API\e[0m."
-echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (GUI only):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 GUI\e[0m."
-echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (API + GUI):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 Both\e[0m."
+echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (API only):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 API\e[0m"
+echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (GUI only):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 GUI\e[0m"
+echo -e "\e[1mTo turn an existing Rails engine into a Thecore one (API + GUI):\e[0m\n  1) please cd into engine directory\n  2) please run \e[31m$0 Both\e[0m"
 echo
 exit 0
 fi
