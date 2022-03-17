44fba8b69eea40a468205e42afa4dc1a98682be0
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-17 11:43:04 +0000

    add localdev build script

---
+++ scripts/localdev-docker-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cd /workspaces/project/app
+bundle update
+SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
+
+cd /etc/thecore/localdockerbuild
+docker-compose build --pull --no-cache backend
+docker-compose up -d
