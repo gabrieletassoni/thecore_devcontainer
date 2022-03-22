442a90778b8591f91d72c12e4eae8639854623b4
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-22 14:05:32 +0000

    fix network

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -13,5 +13,13 @@ docker-compose --version
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 # Building container and pushing to the registry
 cd /etc/thecore/docker/
-docker-compose -f docker-compose.yml -f docker-compose.build.yml build --pull --no-cache backend
-docker-compose -f docker-compose.yml -f docker-compose.build.yml push
+docker-compose \
+    -f docker-compose.yml \
+    -f docker-compose.build.yml \
+    -f docker-compose.net.yml \
+    build --pull --no-cache backend
+docker-compose \
+    -f docker-compose.yml \
+    -f docker-compose.build.yml \
+    -f docker-compose.net.yml \
+    push
