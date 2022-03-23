b7e3415338883a7d8d2ddd0f0b8644063e25e620
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-23 17:20:58 +0000

    add export for BUILD_DIR

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,12 +4,14 @@
 sudo docker version
 sudo docker-compose version
 sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 # Building container and pushing to the registry
 cd /etc/thecore/docker/
 # -f docker-compose.net.yml \
 # -f docker-compose.net.yml \
+export BUILD_DIR="${CI_PROJECT_DIR}"
 sudo docker-compose \
     -f docker-compose.yml \
     -f docker-compose.build.yml \
-    build --pull --no-cache --build-arg "BUILD_DIR=${CI_PROJECT_DIR}" backend
+    build --pull --no-cache backend
 sudo docker-compose push
