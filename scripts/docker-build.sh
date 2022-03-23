bfd5d16ec2f775bdef40cbe055effb584afa8b4e
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-23 18:15:54 +0000

    Use current context to build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,14 +3,17 @@
 # Testing docker installation
 sudo docker version
 sudo docker-compose version
-sudo docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+sudo docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 # Building container and pushing to the registry
 cd /etc/thecore/docker/
 
+DOCKER_SERVICE=${DOCKER_SERVICE:-backend}
+
+export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/$DOCKER_SERVICE:$CI_COMMIT_TAG 
 sudo docker-compose \
     -f docker-compose.yml \
     -f docker-compose.build.yml \
-    build --pull --no-cache --build-arg "BUILD_DIR=${CI_PROJECT_DIR}" backend
+    build --pull --no-cache --build-arg "BUILD_DIR=${CI_PROJECT_DIR}" "$DOCKER_SERVICE"
 
 sudo docker-compose push
