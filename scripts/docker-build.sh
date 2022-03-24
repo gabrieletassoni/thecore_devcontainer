bf02eef32a7eae8ec03231a542d4c369b8b3fc19
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-24 08:28:22 +0000

    Debug localdocker compile

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,12 +8,13 @@ echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 # Building container and pushing to the registry
 cd /etc/thecore/docker/
 
-DOCKER_SERVICE=${DOCKER_SERVICE:-backend}
+export DOCKER_SERVICE=${DOCKER_SERVICE:-backend}
 
-export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/$DOCKER_SERVICE:$CI_COMMIT_TAG 
-sudo docker-compose \
+export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/$DOCKER_SERVICE:$CI_COMMIT_TAG
+
+sudo -E docker-compose \
     -f docker-compose.yml \
     -f docker-compose.build.yml \
     build --pull --no-cache --build-arg "BUILD_DIR=${CI_PROJECT_DIR}" "$DOCKER_SERVICE"
 
-sudo docker-compose push
+sudo -E docker-compose push
--- scripts/thecore-localdev-docker-build.sh
+++ scripts/thecore-localdev-docker-build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 
-docker version
-docker-compose version
+sudo docker version
+sudo docker-compose version
 
 cd /workspaces/project/app
 bundle update
 SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
 
 cd /etc/thecore/localdockerbuild
-docker-compose \
+
+export IMAGE_TAG_BACKEND="backenddev:latest"
+export CI_PROJECT_DIR=/workspaces/project/app
+
+sudo -E docker-compose \
     -f docker-compose.yml \
     -f docker-compose.build.yml \
-    build --pull --no-cache backend
-docker-compose up --remove-orphans
+    build --pull --no-cache --build-arg "BUILD_DIR=${CI_PROJECT_DIR}" backend
+
+sudo -E docker-compose up --remove-orphans
