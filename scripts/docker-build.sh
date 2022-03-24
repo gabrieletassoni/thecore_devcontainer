e95af76ec8d7dfb90ef13a4473c1785b626b4931
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-24 10:27:20 +0000

    fix deploy script

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 
-sudo -iuroot
-
 # Testing docker installation
-docker version
-docker-compose version
-docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+sudo -E docker version
+sudo -E docker-compose version
+sudo -E docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 
 # Building container and pushing to the registry
 cd /etc/thecore/docker/
@@ -15,12 +13,12 @@ export DOCKER_SERVICE=${DOCKER_SERVICE:-backend}
 export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/$DOCKER_SERVICE:$CI_COMMIT_TAG
 
 echo "Building Image $IMAGE_TAG_BACKEND"
-docker-compose \
+sudo -E docker-compose \
     -f docker-compose.yml \
     -f docker-compose.build.yml \
     build --pull --no-cache "$DOCKER_SERVICE"
 
 echo "Pushing Image $IMAGE_TAG_BACKEND"
-docker-compose \
+sudo -E docker-compose \
     -f docker-compose.yml \
     push "$DOCKER_SERVICE"
--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -20,7 +20,7 @@ then
     exit 3
 fi
 
-if [[ $SEMVER == $ENVIRONMENT && $SEMVER == $VERSION && $VERSION == $ENVIRONMENT ]]
+if [[ $SEMVER == "$ENVIRONMENT" && $SEMVER == "$VERSION" && $VERSION == "$ENVIRONMENT" ]]
 then
     # Sono in production
     HOSTFILE="docker_host"
@@ -40,30 +40,33 @@ fi
 cd installers
 for PROVIDER in *
 do 
-if [ -f "$PROVIDER/$HOSTFILE" ]
-then
-    echo "$PROVIDER has a $HOSTFILE file, let's see if it also has customers"
-    export DOCKER_HOST="$(cat "$PROVIDER/$HOSTFILE")"
-    export DOCKER_HOST_DOMAIN="$(echo $DOCKER_HOST | cut -d'/' -f3 | cut -d':' -f1)"
-    export DOCKER_HOST_PORT="$(echo $DOCKER_HOST | cut -d'/' -f3 | cut -d':' -f2)"
-    ssh $DOCKER_HOST_DOMAIN -p $DOCKER_HOST_PORT "
-        docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY; 
-        mkdir -p /tmp/installers; 
-        exit"
-    rsync -arvz -e "ssh -p $DOCKER_HOST_PORT" --progress --delete docker-compose.yml $PROVIDER ${DOCKER_HOST_DOMAIN}:/tmp/installers/
-    for CUSTOMER in $PROVIDER/*.env
-    do
-    echo "  - found $CUSTOMER doing the remote up thing on $DOCKER_HOST"
-    ssh $DOCKER_HOST_DOMAIN -p $DOCKER_HOST_PORT " 
-        export IMAGE_TAG_HELPDESK_SIDEKIQ=$IMAGE_TAG_HELPDESK_SIDEKIQ; 
-        export IMAGE_TAG_HELPDESK=$IMAGE_TAG_HELPDESK; 
-        export IMAGE_TAG_BACKEND=$IMAGE_TAG_BACKEND; 
-        cd /tmp/installers
-        docker-compose --env-file $CUSTOMER pull; 
-        docker-compose --env-file $CUSTOMER up -d --remove-orphans --no-build;
-        docker system prune -f; docker logout $CI_REGISTRY; 
-        exit"
-    done
-fi
+    if [ -f "$PROVIDER/$HOSTFILE" ]
+    then
+        echo "$PROVIDER has a $HOSTFILE file, let's see if it also has customers"
+        DOCKER_HOST="$(cat "$PROVIDER/$HOSTFILE")"
+        export DOCKER_HOST
+        DOCKER_HOST_DOMAIN="$(echo "$DOCKER_HOST" | cut -d'/' -f3 | cut -d':' -f1)"
+        export DOCKER_HOST_DOMAIN
+        DOCKER_HOST_PORT="$(echo "$DOCKER_HOST" | cut -d'/' -f3 | cut -d':' -f2)"
+        export DOCKER_HOST_PORT
+        ssh "$DOCKER_HOST_DOMAIN" -p "$DOCKER_HOST_PORT" "
+            docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY; 
+            mkdir -p /tmp/installers; 
+            exit"
+        rsync -arvz -e "ssh -p $DOCKER_HOST_PORT" --progress --delete /etc/thecore/docker/docker-compose.yml "$PROVIDER" "${DOCKER_HOST_DOMAIN}:/tmp/installers/"
+        for CUSTOMER in "$PROVIDER"/*.env
+        do
+        echo "  - found $CUSTOMER doing the remote up thing on $DOCKER_HOST"
+        ssh "$DOCKER_HOST_DOMAIN" -p "$DOCKER_HOST_PORT" " 
+            export IMAGE_TAG_HELPDESK_SIDEKIQ=$IMAGE_TAG_HELPDESK_SIDEKIQ; 
+            export IMAGE_TAG_HELPDESK=$IMAGE_TAG_HELPDESK; 
+            export IMAGE_TAG_BACKEND=$IMAGE_TAG_BACKEND; 
+            cd /tmp/installers
+            docker-compose --env-file $CUSTOMER pull; 
+            docker-compose --env-file $CUSTOMER up -d --remove-orphans --no-build;
+            docker system prune -f; docker logout $CI_REGISTRY; 
+            exit"
+        done
+    fi
 done
     
