f6c3d3fc31c29a8b1ec21937429748e4382ee9d6
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-25 09:09:24 +0000

    Fix help script and remove unused files

--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 [[ -z "$BANCOLINI_GEMS_CREDENTIALS" ]] && exit 1
+
 bundle config set gems.bancolini.com "$BANCOLINI_GEMS_CREDENTIALS"
-bundle update
+bundle update --verbose
 SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
 rm -rf tmp/cache/* /tmp/*
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash -e
+echo "Testing docker installation."
+docker version
 
-# Testing docker installation
-sudo docker version
-
-echo "Checking ${CI_PROJECT_DIR} content (context for Dockerfile)"
-ls -l "${CI_PROJECT_DIR}"
-ls -l "${CI_PROJECT_DIR}/vendor/bundle/ruby/*/gems/"
+cd "${CI_PROJECT_DIR}"
 
 echo "Building Image $IMAGE_TAG_BACKEND"
-sudo docker build -f /etc/thecore/docker/Dockerfile --no-cache --pull -t "${IMAGE_TAG_BACKEND}" "${CI_PROJECT_DIR}"
+docker build -f /etc/thecore/docker/Dockerfile --no-cache --pull -t "${IMAGE_TAG_BACKEND}" .
 
 echo "Login at $CI_REGISTRY"
-sudo docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 
 echo "Pushing Image $IMAGE_TAG_BACKEND"
-sudo docker image push "${IMAGE_TAG_BACKEND}"
+docker image push "${IMAGE_TAG_BACKEND}"
