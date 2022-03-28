be76c8a0d6b5f5912f1115987b5f63a3f601b2f3
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-03-28 12:50:24 +0000

    fix linting problem from shellcheck recommentation

--- bin/functions.sh
+++ bin/functions.sh
@@ -2,7 +2,7 @@
 
 function yes_or_no {
     while true; do
-        read -p "$* [y/n]: " yn
+        read -rp "$* [y/n]: " yn
         case $yn in
             [Yy]*) return 0  ;;  
             [Nn]*) echo "Aborted" ; return  1 ;;
