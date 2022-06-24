d4ce215fbeac33f341275e93f615711401395a9d
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-06-24 15:24:42 +0000

    add hook for pre compile custom script

--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -2,6 +2,7 @@
 
 bundle config set path vendor/bundle
 bundle config get path
+[[ -f vendor/container/pre-compile.sh ]] && source vendor/container/pre-compile.sh
 bundle update
 SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
 rm -rf tmp/cache/* /tmp/*
