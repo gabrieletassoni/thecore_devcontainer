f51aacec5e7c65868eeb05db4dbca492ef2a8e10
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-03-05 11:06:45 +0000

    Add echo info to bettere understand what's happening in the pipeline

--- scripts/gem-compile.sh
+++ scripts/gem-compile.sh
@@ -25,21 +25,30 @@ then
     echo "Version $version already exists"
 else
     echo "Version $version does not exist"
+    echo "Setting up git user and email"
     git config --local user.email "${GITLAB_EMAIL:-noreply@alchemic.it}"
     git config --local user.name "${GITLAB_USER_NAME:-AlchemicIT}"
+    echo "Creating tag $version"
     git tag -a $version -m "Version $version"
+    echo "Pushing tag $version"
     if [ -z "$GITLAB_OAUTH_TARGET" ]
     then
+        echo "GITLAB_OAUTH_TARGET does not exists. Pushing to origin."
         git push --tags
     else
+        echo "Pushing to $GITLAB_OAUTH_TARGET"
         git push --tags "$GITLAB_OAUTH_TARGET"
     fi
+    echo "Building gem"
     gem build *.gemspec
+    echo "Pushing gem"
     if [ -z "$GITLAB_GEM_REPO_TARGET" ]
     then
+        echo "GITLAB_GEM_REPO_TARGET does not exists. Pushing to rubygems.org."
         gem push
     else
         # This needs also the env var GEM_HOST_API_KEY to be set with the credentials from a valid user.
+        echo "Pushing to $GITLAB_GEM_REPO_TARGET"
         gem push --host "$GITLAB_GEM_REPO_TARGET" *.gem
     fi
 fi
