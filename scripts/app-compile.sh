a9d4731bdb1e7bc79f4019a3e56946066df112d1
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-04-17 15:20:09 +0000

    Get version from version file and remove tag dependency Remove unused files

--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -1,33 +1,75 @@
 #!/bin/bash -e
 
-CURDIR=$(pwd)
-bundle config set path "$CURDIR/vendor/bundle"
-bundle config get path
-echo "Compiling the default image"
-bundle install
-SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
-rm -rf tmp/cache/* /tmp/*
+echo Getting the version from version file
+version=$(tr -d '\n' < version)
+echo Obtain the list of local tags
+git config --global --add safe.directory $CI_PROJECT_DIR
+local_tags=$(git tag)
+echo $local_tags
+echo Obtain the list of remote tags
+remote_tags=$(git ls-remote --tags origin | awk '{print $2}' | awk -F '/' '{print $3}')
+echo $remote_tags
+echo Iterate over local tags
+for tag in $local_tags; do
+    echo Verify if tag $tag has a corresponding remote tag
+    if ! echo "$remote_tags" | grep -q "^$tag$"; then
+        echo This tag does not exist on the remote: $tag
+        git tag -d "$tag"
+        echo "Tag $tag eliminata."
+    fi
+done
+echo If version $version already exists, do nothing
+echo otherwise, create a new tag, push it and build the gem
+if echo $remote_tags | grep -q $version;
+then
+    echo "Version $version already exists"
+else
+    echo "Version $version does not exist"
+    echo "Setting up git user and email"
+    git config --local user.email "${GITLAB_EMAIL:-noreply@alchemic.it}"
+    git config --local user.name "${GITLAB_USER_NAME:-AlchemicIT}"
+    echo "Creating tag $version"
+    git tag -a $version -m "Version $version"
+    echo "Pushing tag $version"
+    if [ -z "$GITLAB_OAUTH_TARGET" ]
+    then
+        echo "GITLAB_OAUTH_TARGET does not exists. Pushing to origin."
+        git push --tags
+    else
+        echo "Pushing to $GITLAB_OAUTH_TARGET"
+        git push --tags "$GITLAB_OAUTH_TARGET"
+    fi
 
-export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend:$CI_COMMIT_TAG
-echo "Building $IMAGE_TAG_BACKEND"
-/usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile"
-
-echo "Compiling custom images"
-TARGETDIR="${CI_PROJECT_DIR:-.}/vendor/custombuilds/"
-[[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name Dockerfile | while read -r file; do
-    echo "Compiling a custom image for: $file";
-    # Looking if thre is a custom script
-    DIRNAME=$(dirname "$file")
-    PRECOMPILESCRIPT="$DIRNAME/pre-compile.sh"
-    [[ -f $PRECOMPILESCRIPT ]] && export `$PRECOMPILESCRIPT`
-    # Looking if there are more gems to add
-    GEMFILEDELTA="$DIRNAME/Gemfile"
-    [[ -f $GEMFILEDELTA ]] && bundle install --gemfile "$GEMFILEDELTA"
-    
+    # A new tag has been added to the repository, so we need to compile the images
+    CURDIR=$(pwd)
+    bundle config set path "$CURDIR/vendor/bundle"
+    bundle config get path
+    echo "Compiling the default image"
+    bundle install
     SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
     rm -rf tmp/cache/* /tmp/*
 
-    export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend-$(basename "$DIRNAME"):$CI_COMMIT_TAG
+    export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend:$CI_COMMIT_TAG
     echo "Building $IMAGE_TAG_BACKEND"
-    /usr/bin/docker-build.sh "$file"
-done
+    /usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile" $version
+
+    echo "Compiling custom images"
+    TARGETDIR="${CI_PROJECT_DIR:-.}/vendor/custombuilds/"
+    [[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name Dockerfile | while read -r file; do
+        echo "Compiling a custom image for: $file";
+        # Looking if thre is a custom script
+        DIRNAME=$(dirname "$file")
+        PRECOMPILESCRIPT="$DIRNAME/pre-compile.sh"
+        [[ -f $PRECOMPILESCRIPT ]] && export `$PRECOMPILESCRIPT`
+        # Looking if there are more gems to add
+        GEMFILEDELTA="$DIRNAME/Gemfile"
+        [[ -f $GEMFILEDELTA ]] && bundle install --gemfile "$GEMFILEDELTA"
+        
+        SECRET_KEY_BASE=dummy RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
+        rm -rf tmp/cache/* /tmp/*
+
+        export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend-$(basename "$DIRNAME"):$CI_COMMIT_TAG
+        echo "Building $IMAGE_TAG_BACKEND"
+        /usr/bin/docker-build.sh "$file" $version
+    done
+fi
