514bf6949747dde5d2ff4cc09b9fbdfc0321f9dd
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2025-01-16 15:51:04 +0000

    Move to the entrypoint the assets precompile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,10 +19,13 @@ then
     echo -e "\e[32m- Doing DB seed\e[0m"
     if bundle exec ./bin/rails thecore:db:seed
     then
-        # Only if all the migrations are ok, run the server
-        echo -e "\e[32m- - Everything was ok, starting the rails server\e[0m"
-        rm -f tmp/pids/server.pid
-        bundle exec ./bin/rails s -p 3000 -b '0.0.0.0'
+        echo -e "\e[32m- - Doing Assets Precompile\e[0m"
+        if bundle exec ./bin/rails assets:precompile
+        then
+            echo -e "\e[32m- - - Everything was ok, starting the rails server\e[0m"
+            rm -f tmp/pids/server.pid
+            bundle exec ./bin/rails s -p 3000 -b '0.0.0.0'
+        fi
     fi
 fi
 
--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -47,8 +47,8 @@ else
     echo "Compiling the default image"
     bundle install
         
-    echo "Compiling the default assets (/ and /backend)"
-    SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
+    # echo "Compiling the default assets (/ and /backend)"
+    # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
     # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/ ASSETS_PREFIX=/assets ./bin/rails --trace assets:precompile
     # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/backend ASSETS_PREFIX=/backend/assets ./bin/rails --trace assets:precompile
 
@@ -58,26 +58,26 @@ else
     echo "Building $IMAGE_TAG_BACKEND"
     /usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile" $version
 
-    echo "Compiling custom images"
-    TARGETDIR="${CI_PROJECT_DIR:-.}/vendor/custombuilds/"
-    [[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name Dockerfile | while read -r file; do
-        echo "Compiling a custom image for: $file";
-        # Looking if thre is a custom script
-        DIRNAME=$(dirname "$file")
-        PRECOMPILESCRIPT="$DIRNAME/pre-compile.sh"
-        [[ -f $PRECOMPILESCRIPT ]] && export `$PRECOMPILESCRIPT`
-        # Looking if there are more gems to add
-        GEMFILEDELTA="$DIRNAME/Gemfile"
-        [[ -f $GEMFILEDELTA ]] && bundle install --gemfile "$GEMFILEDELTA"
+    # echo "Compiling custom images"
+    # TARGETDIR="${CI_PROJECT_DIR:-.}/vendor/custombuilds/"
+    # [[ -d "$TARGETDIR" ]] && find "$TARGETDIR" -name Dockerfile | while read -r file; do
+    #     echo "Compiling a custom image for: $file";
+    #     # Looking if thre is a custom script
+    #     DIRNAME=$(dirname "$file")
+    #     PRECOMPILESCRIPT="$DIRNAME/pre-compile.sh"
+    #     [[ -f $PRECOMPILESCRIPT ]] && export `$PRECOMPILESCRIPT`
+    #     # Looking if there are more gems to add
+    #     GEMFILEDELTA="$DIRNAME/Gemfile"
+    #     [[ -f $GEMFILEDELTA ]] && bundle install --gemfile "$GEMFILEDELTA"
         
-        SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
-        # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/ ASSETS_PREFIX=/assets ./bin/rails --trace assets:precompile
-        # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/backend ASSETS_PREFIX=/backend/assets ./bin/rails --trace assets:precompile
+    #     # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake ./bin/rails --trace assets:precompile
+    #     # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/ ASSETS_PREFIX=/assets ./bin/rails --trace assets:precompile
+    #     # SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production DATABASE_URL=nulldb:fake RAILS_RELATIVE_URL_ROOT=/backend ASSETS_PREFIX=/backend/assets ./bin/rails --trace assets:precompile
         
-        rm -rf tmp/cache/* /tmp/*
+    #     rm -rf tmp/cache/* /tmp/*
 
-        export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend-$(basename "$DIRNAME"):$version
-        echo "Building $IMAGE_TAG_BACKEND"
-        /usr/bin/docker-build.sh "$file" $version
-    done
+    #     export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend-$(basename "$DIRNAME"):$version
+    #     echo "Building $IMAGE_TAG_BACKEND"
+    #     /usr/bin/docker-build.sh "$file" $version
+    # done
 fi
