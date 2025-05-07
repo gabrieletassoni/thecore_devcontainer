38c786a6461a2d35bfd3ef42e4e3a567d09f14cb
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2025-05-07 14:22:17 +0000

    add custom build

--- scripts/app-compile.sh
+++ scripts/app-compile.sh
@@ -14,4 +14,16 @@ rm -rf tmp/cache/* /tmp/*
 
 export IMAGE_TAG_BACKEND=${CI_REGISTRY_IMAGE}/backend:$version
 echo "Building $IMAGE_TAG_BACKEND"
-/usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile" "$version"
+
+# If $CURDIR/Dockerfile exists, use it
+if [ -f "$CURDIR/Dockerfile" ]; then
+    echo "Using Dockerfile in $CURDIR"
+    /usr/bin/docker-build.sh "$CURDIR/Dockerfile" "$version"
+else
+    echo "Using Dockerfile in /etc/thecore/docker"
+    # If $CURDIR/Dockerfile does not exist, use the one in /etc/thecore/docker
+    # This is the default location for the Dockerfile
+    /usr/bin/docker-build.sh "/etc/thecore/docker/Dockerfile" "$version"
+fi
+
+echo "Building $IMAGE_TAG_BACKEND done"
