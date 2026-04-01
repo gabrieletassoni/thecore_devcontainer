2f9d5ae47895070ff7632b432a522f5bfbe7c356
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2026-04-01 16:31:02 +0000

    Add Network Host to docker build command in order to have a cleaner and less layered networking in place using DIND images.

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -10,7 +10,7 @@ DOCKERFILE_LOCATION="$1"
 echo "Using $DOCKERFILE_LOCATION for build"
 DIRS=$(dirname "$1")
 CDELTA=./vendor/custombuilds/$(basename "$DIRS")/
-docker build -f "$DOCKERFILE_LOCATION" --no-cache --pull -t "${IMAGE_TAG_BACKEND}" \
+docker build -f "$DOCKERFILE_LOCATION" --no-cache --pull --network=host -t "${IMAGE_TAG_BACKEND}" \
     --build-arg "CUSTOMBUILDDIR=$CDELTA" \
     --build-arg "CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}" \
     --build-arg "CI_COMMIT_TAG=$2" .
